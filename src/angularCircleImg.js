e0d8c01954dcfc78daa70244319412d34941db2e
Alexander Nied <awnied@gmail.com>
2016-07-04 23:39:38 -0500

    Started fleshing out the directive, but am realizing that the proper modularization step really needs to come sooner in the process to make this easier.

--- src/angularCircleImg.js
+++ src/angularCircleImg.js
@@ -10,6 +10,10 @@
 	function circleImg() {
 		var directive = {};
 		directive.restrict = 'E';
+		directive.scope = {
+			ciSrc : '@',
+			ciDiameter : '@'
+		};
 		directive.link = function (scope, elem, attrs) {
 			console.log('it worked!');
 		};
