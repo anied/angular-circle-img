6f1b82e61ff256d065fcb6c0b4d490b454952d2f
Alexander Nied <awnied@gmail.com>
2016-07-07 07:46:20 -0500

    Pumping through src and alt attributes and loading on the page.

--- src/angularCircleImg.js
+++ src/angularCircleImg.js
@@ -3,10 +3,18 @@ export default function circleImg() {
 	directive.restrict = 'E';
 	directive.scope = {
 		ciSrc : '@',
-		ciDiameter : '@'
+		ciDiameter : '@',
+		ciAlt: '@'
 	};
 	directive.link = function (scope, elem, attrs) {
 		console.log('it worked!');
 	};
+
+	directive.template = `
+		<div class="ci-wrapper">
+			<img src="{{ciSrc}}" alt="{{ciAlt}}">
+		</div>
+	`;
+
 	return directive;
 }
