141cbca581b9f37903b5477f1e89a9d857587ed9
Alexander Nied <awnied@gmail.com>
2016-07-08 00:07:45 -0500

    Working!  At least with the mona lisa test image!

--- src/angularCircleImg.js
+++ src/angularCircleImg.js
@@ -1,5 +1,8 @@
-export default function circleImg() {
+export default function circleImg($timeout) {
 	//TODO-- this could be more ES6-y -- use a class instead
+
+	"ngInject";
+
 	let directive = {};
 	directive.restrict = 'E';
 	directive.scope = {
@@ -17,15 +20,38 @@ export default function circleImg() {
 	    testImg.style.opacity = 0;
 	    testImg.style.top = 0;
 	    testImg.style.left = 0;
+	    testImg.style.margin = 0;
+	    testImg.style.padding = 0;
 
 		docfrag.appendChild(testImg);
 
 		document.body.appendChild(docfrag);
+
+		setTimeout(function () {
+			const imgBounds = testImg.getBoundingClientRect();
+			if (imgBounds.height < imgBounds.width) {
+				scope.height = scope.ciDiameter;
+				scope.width = imgBounds.width * (scope.ciDiameter/imgBounds.height);
+			} else if (imgBounds.width < imgBounds.height) {
+				scope.width = scope.ciDiameter;
+				scope.height = imgBounds.height * (scope.ciDiameter/imgBounds.width);
+			} else if (imgBounds.height === imgBounds.width) {
+				scope.width = scope.ciDiameter;
+				scope.height = scope.ciDiameter;
+			}
+
+			scope.width = parseFloat(scope.width, 10);
+			scope.height = parseFloat(scope.height, 10);
+
+			$timeout(scope.$apply());
+
+		});
+
 	};
 
 	directive.template = `
 		<div class="ci-wrapper" ng-style="{{circleImgCtl.wrapperStyle()}}">
-			<img src="{{ciSrc}}" alt="{{ciAlt}}">
+			<img src="{{ciSrc}}" alt="{{ciAlt}}" height="{{height}}" width="{{width}}">
 		</div>
 	`;
 
