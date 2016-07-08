d8df4945e2e8b9fd91a80140115c38d86a8f3077
Alexander Nied <awnied@gmail.com>
2016-07-07 22:39:09 -0500

    Image wrapper being styled correctly.

--- src/angularCircleImgCtl.js
+++ src/angularCircleImgCtl.js
@@ -3,9 +3,17 @@ class circleImgCtl {
 
 		"ngInject";
 
-		this.wrapperStyle = function () { //seems like maybe this isn't the es6 way to do this?
-			console.log('wrapperStyle');
-			console.log($scope);
+		let vm = this;
+
+		vm.wrapperStyle = function () { //seems like maybe this isn't the es6 way to do this?
+			const diameter = $scope.ciDiameter;
+			const borderRadiusVal = diameter/2;
+			return {
+				'height': diameter.toString() + 'px',
+				'width': diameter.toString() + 'px',
+				'overflow': 'hidden',
+				'border-radius': borderRadiusVal.toString() + 'px'
+			};
 		};
 	}
 
