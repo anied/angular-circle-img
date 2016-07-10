b7b553e364b0a7de31e9363ea82f4053fb451165
Alexander Nied <awnied@gmail.com>
2016-07-09 23:55:30 -0500

    Added balance-- but it is breaking some of the existing index (in as much as the default now looks a bit weird.)

--- src/angularCircleImgCtl.js
+++ src/angularCircleImgCtl.js
@@ -5,6 +5,17 @@ class circleImgCtl {
 
 		let vm = this;
 
+		const BalanceXOptions = ['left', 'center', 'right'];
+		const BalanceYOptions  = ['top', 'center', 'bottom'];
+
+		if ( BalanceXOptions.indexOf($scope.ciBalanceX) === -1 ) { // if balance is omitted or malformed, we just assume 'center'
+			$scope.ciBalanceX = 'center';
+		}
+
+		if ( BalanceYOptions.indexOf($scope.ciBalanceY) === -1 ) { // if balance is omitted or malformed, we just assume 'center'
+			$scope.ciBalanceY = 'center';
+		}
+
 		vm.wrapperStyle = function () { //seems like maybe this isn't the es6 way to do this?
 			const diameter = $scope.ciDiameter;
 			const borderRadiusVal = diameter/2;
@@ -12,8 +23,37 @@ class circleImgCtl {
 				'height': diameter.toString() + 'px',
 				'width': diameter.toString() + 'px',
 				'overflow': 'hidden',
-				'border-radius': borderRadiusVal.toString() + 'px'
+				'border-radius': borderRadiusVal.toString() + 'px',
+				'position': 'relative'
 			};
+		};
+
+		vm.imgStyle = function () {
+			let imgStyleObj = {};
+			const nonCenterXFlag = $scope.ciBalanceX !== 'center'; // Flag stating the X value is not 'center'
+			const nonCenterYFlag = $scope.ciBalanceY !== 'center'; // Flag stating the Y value is not 'center'
+
+			// if ( nonCenterXFlag || nonCenterYFlag ) { 	// If either axis is not to be centered (as by default)...
+				imgStyleObj.position = 'absolute';		// position the image absolute
+
+				if (nonCenterXFlag) {
+					imgStyleObj[$scope.ciBalanceX] = 0;
+				} else {
+					const pos = ((-($scope.width/2)) + ($scope.ciDiameter/2))+ 'px';
+					imgStyleObj.left = pos;
+				}
+
+				if (nonCenterYFlag) {
+					imgStyleObj[$scope.ciBalanceY] = 0;
+				} else {
+					const pos = ((-($scope.height/2)) + ($scope.ciDiameter/2))+ 'px';
+					imgStyleObj.top = pos;
+				}
+
+			// }
+
+			return imgStyleObj;
+
 		};
 	}
 
